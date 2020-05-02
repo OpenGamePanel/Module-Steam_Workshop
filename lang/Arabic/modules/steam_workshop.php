72fe54644b33ebfc270107fe4a253f11ddb91ff4
own3mall <own3mall@gmail.com>
2020-05-02 03:57:59 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Arabic/modules/steam_workshop.php
+++ lang/Arabic/modules/steam_workshop.php
@@ -35,7 +35,7 @@ define('OGP_LANG_connection_error', "خطأ في الإتصال");
 define('OGP_LANG_install_mod', "تثبيت الوحدات");
 define('OGP_LANG_show_mod_info', "Show mods info");
 define('OGP_LANG_select_game', "حدد اللعبة");
-define('OGP_LANG_save_config', "Save Config");
+define('OGP_LANG_save_config', "حفظ التكوين");
 define('OGP_LANG_mod_key_not_found_from_xml', "Mod key %s not found from xml.");
 define('OGP_LANG_workshop_id', "Workshop ID");
 define('OGP_LANG_workshop_id_info', "You'll find the Workshop ID at the URL of the Workshop, for example 440900 for Conan Exiles");
@@ -51,7 +51,7 @@ define('OGP_LANG_place_after', "مكان بعد");
 define('OGP_LANG_place_after_info', "The section of the configuration file where the mods list appears, if any. It will be added to the config file if does not exists yet. If the given variable is not present then it will be placed in the line after this section.");
 define('OGP_LANG_mod_string', "Mod String");
 define('OGP_LANG_mod_string_info', "The string that represents the mod in the mod list. Valid replacements: %workshop_mod_id%, %first_file% (first file is the first file found in the mod folder downloaded by SteamCMD)");
-define('OGP_LANG_string_separator', "String Separator");
+define('OGP_LANG_string_separator', "فاصل السلسلة");
 define('OGP_LANG_string_separator_info', "The character that separates the mods in the configuration file, for example new line character (\\n) or coma (,).");
 define('OGP_LANG_filepath', "مسار الملف");
 define('OGP_LANG_filepath_info', "The path of the configuration file where the mods must be listed.");
@@ -60,7 +60,7 @@ define('OGP_LANG_post_install_info', "The necessary commands in bash to move the
 define('OGP_LANG_install_mods', "Install Mods");
 define('OGP_LANG_uninstall_mods', "Uninstall Mods");
 define('OGP_LANG_failed_uninstalling_mod', "Failed uninstalling mod %s");
-define('OGP_LANG_uninstall', "Uninstall Script");
+define('OGP_LANG_uninstall', "إلغاء تثبيت البرنامج النصي");
 define('OGP_LANG_uninstall_info', "This is the script called when a mod is uninstalled, Valid replacements: %mods_full_path% (the full path to the wokshop mods folder), %mod_string% (mod string is the name listed in the configuration file for this mod).");
 define('OGP_LANG_remove_mods', "Remove Mods");
 define('OGP_LANG_do_not_close_this_page_while_mods_are_being_installed', "Do not close this page while mods are being installed.");
@@ -68,7 +68,7 @@ define('OGP_LANG_no_game_server_selected', "لم يتم تحديد خادم ال
 define('OGP_LANG_there_are_no_mods_installed_on_this_game_server', "There are no mods installed on this game server");
 define('OGP_LANG_workshop_configuration_not_found', "Workshop configuration not found");
 define('OGP_LANG_download_method', "Download Method");
-define('OGP_LANG_anonymous_login', "Anonymous Login");
+define('OGP_LANG_anonymous_login', "تسجيل دخول كمجهول");
 define('OGP_LANG_select_at_least_one_mod_or_enter_mod_id', "Select at least one mod or enter a mod ID.");
-define('OGP_LANG_no_game_servers_assigned', "You don't have any servers assigned to your account.");
+define('OGP_LANG_no_game_servers_assigned', "ليس لديك أي خوادم مخصصة لحسابك.");
 ?>
